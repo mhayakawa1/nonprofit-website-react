dc1757dd2dd327953164a3ca897ec7c0f3a2b5b0
makihaya <93355988+makihaya@users.noreply.github.com>
2022-12-27 20:29:42 -0700

    Create Main.jsx

---
+++ src/components/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+//import video from '../assets/Pexels_Videos_2491282_Trees_Aerial_View.mp4'
+import video from '../assets/Pexels_Videos_4097_Drone_Shot_Of_Forest.mp4'
+
+const Video = () =>{
+    return(
+        <div>
+            <video className='video' src={video} autoPlay loop muted />
+        </div>
+    )
+}
+export default Video
