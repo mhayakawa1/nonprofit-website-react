fde6581503b9ed2c59f4eb01257222d96603c31b
makihaya <93355988+makihaya@users.noreply.github.com>
2023-02-17 21:11:04 -0700

    Update Video.jsx

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -4,9 +4,7 @@ import video from '../assets/Pexels_Videos_4097_Drone_Shot_Of_Forest.mp4'
 const Video = () =>{
     return(
         <div>
-            <video className='video' autoPlay loop muted playsInline>
-                <source src={video} type='video/mp4' />
-            </video>
+            <video className='video' src={video} autoPlay loop muted />
         </div>
     )
 }
