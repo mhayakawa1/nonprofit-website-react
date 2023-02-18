c729e0557cb91c7f2e717ddcd8280ff96788d93d
makihaya <93355988+makihaya@users.noreply.github.com>
2023-02-17 17:16:41 -0700

    Update Video.jsx

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -4,7 +4,18 @@ import video from '../assets/Pexels_Videos_4097_Drone_Shot_Of_Forest.mp4'
 const Video = () =>{
     return(
         <div>
-            <video className='video' src={video} autoPlay loop muted />
+            {/*<video className='video' src={video} autoPlay loop muted />
+            
+  width: 120%;
+  height: 70%;*/}
+            <div className="video"
+          dangerouslySetInnerHTML={{
+            __html: `<video autoplay loop muted playsinline>
+      <source className="" src=${video} type="video/mp4" />
+      Your browser does not support the video tag.
+</video>`,
+          }}
+        />
         </div>
     )
 }
