e8eb18c93deb8398299305401d2f8c871ba1bb7a
makihaya <93355988+makihaya@users.noreply.github.com>
2023-02-17 17:43:36 -0700

    Update Video.jsx

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -4,10 +4,16 @@ import video from '../assets/Pexels_Videos_4097_Drone_Shot_Of_Forest.mp4'
 const Video = () =>{
     return(
         <div>
-            <video className='video' autoPlay loop muted>
-                <source src={video} type='video/mp4' />
-            </video>
+            
+            <div className="fullscreen-video-wrap" dangerouslySetInnerHTML={{ __html: `
+            <video className='video' autoPlay loop muted playsInline>
+                <source src="${video}" type="video/mp4" />
+            </video>            
+        ` }}></div>
         </div>
     )
 }
 export default Video
+{/*<video className='video' autoPlay loop muted playsInline>
+                <source src={video} type='video/mp4' />
+            </video>*/}
