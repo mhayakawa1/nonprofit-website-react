f3262fd4f560746e91f1a48d7014a01d35dab6ba
makihaya <93355988+makihaya@users.noreply.github.com>
2024-01-02 22:53:10 -0700

    Update HamburgerMenu.jsx

--- src/components/HamburgerMenu.jsx
+++ src/components/HamburgerMenu.jsx
@@ -18,30 +18,21 @@ export default class Menu extends React.Component {
   }
   
     render(){
-      const menuHidden = //const for menu with className hidden
-        <ul className='menu-items hidden font-weight-thin'>
-          <a href='#intro-section' className='menu-link home-logo-row' ><img className='menu-link home-logo' src={homeLogo} /></a>
-          <a href='#about-section'><li className='menu-link'>About</li></a>
-          <a href='#our-work-section'><li className='menu-link'>Our Work</li></a>
-          <a href='#get-involved-section'><li className='menu-link last-link'>Get Involved</li></a>
-        </ul>
-      const menuVisible = //const for menu with className visible
-        <ul className='menu-items visible font-weight-thin'>
-          <a href='#intro-section' className='menu-link home-logo-row' ><img className='menu-link home-logo' src={homeLogo} /></a>
-          <a href='#about-section'><li className='menu-link'>About</li></a>
-          <a href='#our-work-section'><li className='menu-link'>Our Work</li></a>
-          <a href='#get-involved-section'><li className='menu-link last-link'>Get Involved</li></a>
-        </ul>
       return( 
       //when user clicks on hamburger menu, call toggleMenu
-      //if showMenu is true, menu is visible, if flalse menu is hidden
+      //if showMenu is true, menu is visible, if false menu is hidden
         <div id='menu'>
-            <div onClick={this.toggleMenu} className='menu-lines'>
-              <span className='menu-line' />
-              <span className='menu-line' />
-              <span className='menu-line' />
-            </div>
-          {this.state.showMenu === true ? menuVisible : menuHidden}
+          <div onClick={this.toggleMenu} className='menu-lines'>
+            <span className='menu-line' />
+            <span className='menu-line' />
+            <span className='menu-line' />
+          </div>
+          <ul className={`menu-items ${this.state.showMenu === true ? 'visible' : 'hidden'} font-weight-thin`}>
+            <a href='#intro-section' className='menu-link home-logo-row' ><img className='menu-link home-logo' src={homeLogo} /></a>
+            <a href='#about-section'><li className='menu-link'>About</li></a>
+            <a href='#our-work-section'><li className='menu-link'>Our Work</li></a>
+            <a href='#get-involved-section'><li className='menu-link last-link'>Get Involved</li></a>
+          </ul>
         </div>
       )
     }
