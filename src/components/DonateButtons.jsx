11ae37962c787df2704cedf4a0429d307647df65
M. Hayakawa <93355988+mhayakawa1@users.noreply.github.com>
2024-11-21 08:32:47 -0700

    Update DonateButtons.jsx

--- src/components/DonateButtons.jsx
+++ src/components/DonateButtons.jsx
@@ -9,53 +9,55 @@ const buttonsDefault = {
   "$200": false
 }
 
-const DonateButton = () => {
-  
+const DonateButton = ({amount, toggleButtons, active}) => {
+  return (
+    <button onClick={event => toggleButtons(event, amount)}
+      className={`donate-amount ${active && 'donate-amount-selected'}`}>{amount}</button>
+  )
 }
 
 export default class DonateButtons extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {...buttonsDefault}
-      this.toggleButtons = this.toggleButtons.bind(this)
-    }
-  
-    toggleButtons(event, dollarAmount){
-      this.setState( state => ({...buttonsDefault}))
+  constructor(props) {
+    super(props);
+    this.state = { ...buttonsDefault }
+    this.toggleButtons = this.toggleButtons.bind(this)
+  }
 
-      if(dollarAmount !== 'custom'){
-        this.setState( state => ({
+  toggleButtons(event, dollarAmount) {
+    this.setState(state => ({ ...buttonsDefault }))
+
+    if (dollarAmount !== 'custom') {
+      this.setState(state => ({
         [dollarAmount]: !this.state[dollarAmount]
       }));
-      }
-    }
-
-    getDonateButtons(){
-      let buttons = [];
-
-      for (const key of Object.keys(this.state)) {
-        buttons.push(
-          <button key={key} onClick={event => this.toggleButtons(event, key)} 
-          className={`donate-amount ${this.state[key] && 'donate-amount-selected'}`}>{key}</button>
-        )
-      }
-      return(
-        buttons
-      )
-    }
-    render(){
-  
-      return(
-        <div className='donate-selection'>
-          <div className='donate-buttons'>
-            {this.getDonateButtons()}
-            <div className='donate-custom'>
-              <label className='dollar-label'>$</label>
-              <input onChange={event => this.toggleButtons(event, 'custom')} className='custom-input' type='number' min='0' 
-                placeholder='Custom' />
-            </div>
-          </div>          
-        </div>
-      )
     }
   }
+
+  getDonateButtons() {
+    let buttons = [];
+
+    for (const key of Object.keys(this.state)) {
+      buttons.push(
+        <DonateButton key={key} amount={key} toggleButtons={this.toggleButtons} active={this.state[key]} />
+      )
+    }
+    return (
+      buttons
+    )
+  }
+  render() {
+
+    return (
+      <div className='donate-selection'>
+        <div className='donate-buttons'>
+          {this.getDonateButtons()}
+          <div className='donate-custom'>
+            <label className='dollar-label'>$</label>
+            <input onChange={event => this.toggleButtons(event, 'custom')} className='custom-input' type='number' min='0'
+              placeholder='Custom' />
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
